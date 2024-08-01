0ffa8f5cd524f0d91474d2b19760e0eecdc07ddc
Asif Khan <beingasifjamshed777@gmail.com>
2024-08-01 22:57:40 +0530

    JS as a First-class Function based prgramming language

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,32 @@
 console.log("ASIF KHAN");
+/*JavaScript is First-class Function based programming language */
+
+/* Assigning a function to a variable */
+
+const func = () => {
+  console.log("This function is assigned to a variable. This function is also a anonymous function");
+};
+// func();
+
+/* This function is  assigned as an Anonymous Function in a Variable named (func), then  used that variable to invoke the function by adding parentheses () at the end */
+
+/* const func = arrow =() => {
+   console.log("This function is assigned to a variable. This function is a named function");
+};
+arrow(); */
+
+/*Even ifthe function is named, you can use the variable name or function name to invoke it.*/
+
+/* Passing a function as an argument */
+
+function func1() {
+  return "Hello";
+}
+
+function func2(func3, name) {
+  console.log(func3(), name);
+}
+
+// func2(func1, "func2");
+
+/* This also explains how we are treating a function as a value */
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # javascript
 A code repo for learning javascript 
+
+## JavaScript is First-class Function programming Language
+A programming language is said to have First-class functions when functions in that 
+
+language are treated like any other variable. For example, in such a language, a function 
+
+can be passed as an argument to other functions, can be returned by another function and 
+
+can be assigned as a value to a variable.
