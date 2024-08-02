1a378f08f141c95df7181e5b833cade0136d586b
Asif Khan <beingasifjamshed777@gmail.com>
2024-08-02 23:08:05 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all the JS code as a newer JS code
+
+// priceValue = 1090;
+// console.log(priceValue);
+
+/* This code will not run because of strict mode if it does not use of it, priceValue variable becomes a global variable and code runs.
+ */
+
+// alert("This is JS learning curve"); I am nodejs not browser
+
+console.log(10 + 20); //code readability should be high
+console.log("Asif");
+
+let name = "Asif Khan";
+let age = 20;
+let isLoggedIn = false;
+ 
+//number => 2 to power 53(Range)
+//bigInt
+//strin =>""
+//boolean => true / false
+//null => dataType+standalone value =>(it's object type)
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof undefined); // It's type is undefined 
+console.log(typeof null); // It's type is object 
