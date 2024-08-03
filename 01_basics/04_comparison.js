2f4c44e5ee0d66536ba3d0a1a102aacb9e2e9704
Asif Khan <beingasifjamshed777@gmail.com>
2024-08-03 20:55:45 +0530

    Ternary Operator

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -15,7 +15,7 @@ console.log("5" >= 2); */
 console.log(null == 0);
 console.log(null >= 0); */
 
- /*
+/*
 The reason is that an equality check == and comparisions "> < >= <=" work differently. 
 Comparision converts null to a number, treating it as 0.
 That's why (3rd) null >= 0 is true and (1th) null > 0 is false. 
@@ -27,7 +27,26 @@ console.log(undefined >= 0); */
 
 //===
 
-console.log("2" === 2);
-
+// console.log("2" === 2);
 
 //Master of  Objects AND Browser's web events
+
+
+
+
+/* ternary operator */
+
+/* let age = 21;
+let result = age >= 18 ? "can drive" : "can't drive";
+console.log(result); 
+
+function getFee(isMember) {
+  return isMember ? "$2.00" : "$10.00";
+}
+
+console.log(getFee(true));
+
+console.log(getFee(false));
+
+console.log(getFee(null));
+*/
