08270417cc7462c18a601ea89a035ae4f4afcdcb
Asif Khan <beingasifjamshed777@gmail.com>
2024-08-06 22:23:27 +0530

    maths and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+// ++++++++++ Numbers +++++++++++
+
+const score = 1000;
+// console.log(score);
+
+const balance = new Number(10000);
+/* console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 133.6789;
+console.log(otherNumber.toPrecision(4));
+
+const bigValue = 1000000;
+console.log(bigValue.toLocaleString("en-IN")); */
+
+// ++++++++++ Maths +++++++++++
+
+/* console.log(Math);
+console.log(Math.abs(-44));
+console.log(Math.round(4.6));
+console.log(Math.ceil(14.1));
+console.log(Math.floor(7.9));
+console.log(Math.min(1, 2, 34, 5, 6, 7, 8, 9));
+console.log(Math.max(1, 2, 34, 5, 6, 7, 8, 9)); */
+
+// console.log(Math.random());
+// console.log(Math.random() * 10 + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
