ed23550090f693c3de0a437c374bf914a104986d
Asif Khan <beingasifjamshed777@gmail.com>
2024-09-14 22:00:00 -0700

    work is still pending on the Todo app

--- 07_projects/6-ToDo App/app.js
+++ 07_projects/6-ToDo App/app.js
@@ -1,80 +1,76 @@
-let data = JSON.parse(localStorage.getItem("allTodo")) || [];
+//This code retrieving the todos and returns an empty array if AllTodos is null
+let data = JSON.parse(localStorage.getItem("AllTodos")) || [];
 
-function displayTodo() {
-  document.getElementById("allTodo").innerHTML = "";
-  data.forEach((singleTodo, idx) => {
-    todoStructure(singleTodo, idx);
+
+
+//This function is for adding new Todo
+function addTodoEelement() {
+  document.getElementById("addBtn").addEventListener("click", (e) => {
+    //This line of code is extracting vale from input field
+    let inputVal = document.getElementById("todoInput").value;
+    e.preventDefault();
+    console.log(inputVal);
+
+    //This code means todo is pushed in the array
+    data.push(inputVal);
+
+    //This function calls here for adding new Todo
+    let newDiv = createTodoElement(inputVal);
+
+    //This line adding a new Todo
+    document.getElementById("allTodo").appendChild(newDiv);
+
+    //localStorage to store todos locally
+    localStorage.setItem("AllTodos", JSON.stringify(data));
+
+    //This line will clear input field
+    document.getElementById("todoInput").value = "";
   });
 }
 
-//This is todo's structure
-function todoStructure(singleTodo, idx) {
-  let div = createElement(singleTodo);
-
-  //This code for remove button
-  let removeBtn = div.getElementsByTagName("button");
-  removeBtn[0].addEventListener("click", (e) => {
-    e.target.parentNode.remove();
-    removeTodo(idx);
+//This function is for delete the Todo
+function delteTodo() {
+  document.getElementById("allTodo").addEventListener("click", (e) => {
+    if (e.target && e.target.classList.contains("deletebtn")) {
+      console.log(e.target.parentNode);
+      e.target.parentNode.remove();
+    }
   });
-
-  //This code for checkbox
-  let checkBox = div.getElementsByTagName("input");
-  checkBox[0].addEventListener("click", (e) => {
-    e.target.previousElementSibling.classList.toggle("completed");
-    toggleTodo(idx);
-  });
-
-  document.getElementById("allTodo").appendChild(div);
 }
 
-//This is event listener on Add button to add Todo
-document.getElementById("addBtn").addEventListener("click", (e) => {
-  e.preventDefault();
+//This function is for checkbox's functionality
+function markTodo() {
+  document.getElementById("allTodo").addEventListener("click", (e) => {
+    if (e.target && e.target.classList.contains("check")) {
 
-  let todoInputValue = document.getElementById("todoInput").value;
 
-  data.push({todoInputValue, completed:false});
+      //closest method searches upward in DOM tree and returns first ancestor matches specified selector
+      let todoDiv = e.target.closest(".singletodo");
+      let headings = todoDiv.querySelectorAll("h3");
 
-  localStorage.setItem("allTodo", JSON.stringify(data));
+      headings.forEach((element) => {
+        element.classList.toggle("marked");
+      });
+      console.log(headings);
+    }
+  });
+}
 
-  displayTodo();
-  //Code to clear input field
-  document.getElementById("todoInput").value = "";
-});
-
-//This function is for creating an element/Todo
-function createElement(singleTodo) {
+//This function creates a new Todo
+function createTodoElement(inputVal) {
   let div = document.createElement("div");
-
-  div.setAttribute("class", "singleTodo");
-
-  div.innerHTML = ` <h2>${singleTodo.todoInputValue}</h2>
-
-  <input type="checkbox" id="checkBox" />
-
-  <button >Remove Todo</button> `;
-
+  div.setAttribute("class", "singletodo");
+  div.innerHTML = `<h3>${inputVal}</h3>
+      <div class="checkbox-container">
+          <input type="checkbox" id="checkbox1" class="check"  />
+          <label for="checkbox1"></label>
+      </div>
+        <button id="removebtn" class="deletebtn">Remove</button>`;
   return div;
 }
 
-//This function is for removing todo from array
-function removeTodo(idx) {
-console.log(idx);
-data.splice(idx, 1)
-localStorage.setItem("allTodo", JSON.stringify(data));
-displayTodo();
+addTodoEelement(); //Call addTodoElement function to add
 
+delteTodo(); //Call delteTodo function to delete
 
-}
-
-//This function is for checkbox functionality
-
-function toggleTodo(idx) {
-  data[idx].completed = !data[idx].completed
-  localStorage.setItem("allTodo", JSON.stringify(data));
-  
-
-}
-
-displayTodo();
+markTodo(); //Call markTodo function to mark todo
--- 07_projects/projectset-1.md
+++ 07_projects/projectset-1.md
@@ -301,6 +301,7 @@ document.querySelector("#restart").addEventListener("click", startGame);
 ### Project -6
 
 ```JavaScript
+//=============== Method-1 ===============
 let data = JSON.parse(localStorage.getItem("allTodo")) || [];
 
 function displayTodo() {
@@ -381,4 +382,85 @@ function toggleTodo(idx) {
 }
 
 displayTodo();
+
+//=============== Method-2 ===============
+
+//This code retrieving the todos and returns an empty array if AllTodos is null
+let data = JSON.parse(localStorage.getItem("AllTodos")) || [];
+
+
+
+//This function is for adding new Todo
+function addTodoEelement() {
+  document.getElementById("addBtn").addEventListener("click", (e) => {
+    //This line of code is extracting vale from input field
+    let inputVal = document.getElementById("todoInput").value;
+    e.preventDefault();
+    console.log(inputVal);
+
+    //This code means todo is pushed in the array
+    data.push(inputVal);
+
+    //This function calls here for adding new Todo
+    let newDiv = createTodoElement(inputVal);
+
+    //This line adding a new Todo
+    document.getElementById("allTodo").appendChild(newDiv);
+
+    //localStorage to store todos locally
+    localStorage.setItem("AllTodos", JSON.stringify(data));
+
+    //This line will clear input field
+    document.getElementById("todoInput").value = "";
+  });
+}
+
+//This function is for delete the Todo
+function delteTodo() {
+  document.getElementById("allTodo").addEventListener("click", (e) => {
+    if (e.target && e.target.classList.contains("deletebtn")) {
+      console.log(e.target.parentNode);
+      e.target.parentNode.remove();
+    }
+  });
+}
+
+//This function is for checkbox's functionality
+function markTodo() {
+  document.getElementById("allTodo").addEventListener("click", (e) => {
+    if (e.target && e.target.classList.contains("check")) {
+
+
+      //closest method searches upward in DOM tree and returns first ancestor matches specified selector
+      let todoDiv = e.target.closest(".singletodo");
+      let headings = todoDiv.querySelectorAll("h3");
+
+      headings.forEach((element) => {
+        element.classList.toggle("marked");
+      });
+      console.log(headings);
+    }
+  });
+}
+
+//This function creates a new Todo
+function createTodoElement(inputVal) {
+  let div = document.createElement("div");
+  div.setAttribute("class", "singletodo");
+  div.innerHTML = `<h3>${inputVal}</h3>
+      <div class="checkbox-container">
+          <input type="checkbox" id="checkbox1" class="check"  />
+          <label for="checkbox1"></label>
+      </div>
+        <button id="removebtn" class="deletebtn">Remove</button>`;
+  return div;
+}
+
+addTodoEelement(); //Call addTodoElement function to add
+
+delteTodo(); //Call delteTodo function to delete
+
+markTodo(); //Call markTodo function to mark todo
+
+
 ```
